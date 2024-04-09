6fdcb673eaf0b9f8a4980a6eaa8f6aca1fdf99fe
itsguitar <guitargmd@gmail.com>
2024-04-10 00:55:10 +0700

    Implement simple score func.

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -1,10 +1,29 @@
 const toggleButton = document.querySelector("#toggleButton");
 
+//TA image
+const main_image1 = document.querySelector("#image_ta1");
+const main_image2 = document.querySelector("#image_ta2");
+
+//Score of each TA
+const count1 = document.querySelector("#ta1");
+const count2 = document.querySelector("#ta2");
+var score1 = {value: 0};
+var score2 = {value: 0};
+
+
 console.log("Ready");
 toggleButton.addEventListener("click", () => {
     switchTA();
-  });
+});
 
+main_image1.addEventListener("mousedown",() => {
+    addScore(main_image1,score1,count1);
+});
+main_image2.addEventListener("mousedown",() => {
+    addScore(main_image2,score2,count2);
+});
+
+//switch team TA JomnoiZ <-> TA Oat
 function switchTA(){
     const score_ta1 = document.querySelector("#ta1");
     const score_ta2 = document.querySelector("#ta2");
@@ -23,4 +42,10 @@ function switchTA(){
         image_ta1.classList.remove("hidden");
         image_ta2.classList.add("hidden");
     }
+}
+
+//increment score
+function addScore(image,score,count){
+    score.value++;
+    count.innerHTML = parseInt(score.value);
 }
