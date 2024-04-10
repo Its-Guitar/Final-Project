4c2f36f982cd0364c43caac91f71bcbd4545dceb
itsguitar <guitargmd@gmail.com>
2024-04-11 01:34:21 +0700

    Fix style

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -7,6 +7,10 @@ const main_image2 = document.querySelector("#image_ta2");
 //Score of each TA
 const count1 = document.querySelector("#ta1");
 const count2 = document.querySelector("#ta2");
+
+//Pop sound
+const popSound = document.getElementById("popSound"); 
+
 var score1 = {value: 0};
 var score2 = {value: 0};
 
@@ -19,6 +23,9 @@ toggleButton.addEventListener("click", () => {
 main_image1.addEventListener("mousedown",() => {
     main_image1.src = "../resources/TA_1_pop.png";
     addScore(main_image1,score1,count1);
+    popSound.currentTime = 0;
+    popSound.play();
+    main_image1.setAttribute("draggable", false);
 });
 
 main_image1.addEventListener("mouseup",() => {
@@ -26,6 +33,9 @@ main_image1.addEventListener("mouseup",() => {
 });
 main_image2.addEventListener("mousedown",() => {
     addScore(main_image2,score2,count2);
+    popSound.currentTime = 0;
+    popSound.play();
+    main_image2.setAttribute("draggable", false);
 });
 
 //switch team TA JomnoiZ <-> TA Oat
